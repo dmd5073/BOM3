ce7d184edcec09c694348018c8ab487c2e60dac8
dmd5073 <dmd5073@gmail.com>
2013-03-24 16:08:41 -0300

    Update bomcreate.py

--- bomcreate.py
+++ bomcreate.py
@@ -23,9 +23,9 @@ youenter.youenter(ClientWON, "Please Renter Your Client's Workorder Number:")
 print "Now that you have provide the information require to setup the Bill of Material file."
 print "Please provide the following design parameters." 
 
-while (yesno.yesno("Will you have mutiple drawing that you will be adding a Bill of Material too?(Y/N)")):
+if(not yesno.yesno("Will you have mutiple drawing that you will be adding a Bill of Material too?(Y/N)")):
+    mydesign = []
     if (yesno.yesno("Will your design information change from drawing to drawing?(Y/N)")):
-       mydesign = []
        while (True):
             DWGNO = raw_input("Please Enter the Drawing Number where the Bill of Material item will be located:")
             youenter.youenter(DWGNO, "Please Renter Your Drawing Number:")
@@ -39,8 +39,23 @@ while (yesno.yesno("Will you have mutiple drawing that you will be adding a Bill
             mydesign.append(Design(DWGNO,DP,DF))
 
             if(not yesno.yesno("Would you like to add another drawing?(Y/N")):
-                break;
-        print "WES ADDED THIS LINE"
+                break
+    elif (yesno.yesno("Will your design information stay the same?(Y/N)")):
+        DP =  int(raw_input ("Please Enter Your Design Pressure:"))
+        youenter.youenter(DP, "Please Renter Your Design Pressure:")
+
+        DF = float(raw_input ("Please Enter Your Design Factor:"))
+        youenter.youenter(DF, "Please Renter Your Design Factor:")
+        
+        while (True):
+            DWGNO = raw_input("Please Enter the Drawing Number where the Bill of Material item will be located:")
+            youenter.youenter(DWGNO, "Please Renter Your Drawing Number:")
+            
+            mydesign.append(Design(DWGNO,DP,DF))
+            
+            if(not yesno.yesno("Would you like to add another drawing?(Y/N)")):
+                break
+        
     else:
         print "I Work"
         break
